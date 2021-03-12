7def21ca77a98153813935f9c79a61936e0ba37f
janwes <hzwjanwes@outlook.com>
2021-03-12 17:54:22 +0800

    first commit

---
+++ src/main/java/com/janwes/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.janwes.controller;
+
+/**
+ * @author Janwes
+ * @version 1.0
+ * @package com.janwes.controller
+ * @date 2021/3/12 17:38
+ * @description
+ */
+public class OrderController {
+    public static void main(String[] args) {
+        System.out.println("print order");
+    }
+}
